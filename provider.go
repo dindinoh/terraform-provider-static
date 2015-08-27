fe75a71a3c14892af01049f5593f3d5e88ea25a3
Fredrik Gustafsson <gusten@dindinoh.net>
2015-08-27 21:29:15 +0200

    first complete build

---
+++ provider.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+//Provider is good
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{},
+	}
+}

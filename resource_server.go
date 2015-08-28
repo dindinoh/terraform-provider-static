bf24aee3f4aa37476d10cda4af0c8165e9d0b01f
Fredrik Gustafsson <gusten@dindinoh.net>
2015-08-28 22:12:06 +0200

    nasty crash fixed .. missed resources of course. and a way to re-execute provisioner found

--- resource_server.go
+++ resource_server.go
@@ -7,9 +7,9 @@ import (
 func resourceServer() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceServerCreate,
-		// Read:   resourceServerRead,
+		Read:   resourceServerRead,
 		Update: resourceServerUpdate,
-		// Delete: resourceServerDelete,
+		Delete: resourceServerDelete,
 
 		Schema: map[string]*schema.Schema{
 			"address": &schema.Schema{
@@ -30,14 +30,14 @@ func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
 
-// func resourceServerRead(d *schema.ResourceData, m interface{}) error {
-// 	return nil
-// }
+func resourceServerRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
 
 func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
 
-// func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
-// 	return nil
-//}
+func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
